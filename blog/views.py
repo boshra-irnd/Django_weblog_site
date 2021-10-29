a1b402907cd72fe4cd0ea33eaf96ed2efda391c0
Boshra Irandoust <boshra.irnd@gmail.com>
2021-10-29 16:42:19 +0330

    complete comment form

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django import forms
 from django.core import paginator
 from django.db.models import query
+from django.db.models.query_utils import PathInfo
 from django.shortcuts import render , get_object_or_404
 from .models import Post , Comment
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm , CommentForm
 from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -22,15 +24,18 @@ class PostListView(ListView):
 #/2021/3/20/second-blog
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,slug=post,status="published",publish__year=year,publish__month=month,publish__day=day)
+    comments = post.comments.filter(active=True)
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.save()
+            return HttpResponseRedirect(request.path_info)
     else:
         comment_form = CommentForm()
-    return render(request,'blog/post/detail.html',{"post":post, 'form':comment_form})
+    context = {'post':post,'form':comment_form ,'comments':comments}
+    return render(request,'blog/post/detail.html',context)
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status="published")
