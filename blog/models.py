d559557778b03e54d345c15804ad3442eb1ac179
Boshra Irandoust <boshra.irnd@gmail.com>
2021-10-31 17:42:20 +0330

    add blog_tags

--- blog/models.py
+++ blog/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
     tags = TaggableManager()
-    
+
     def get_absolute_url(self):
         return reverse("blog:post_detail",args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
 
