07434f051bb9f57802a59eefe0a34f37a79505cd
Boshra Irandoust <boshra.irnd@gmail.com>
2021-10-31 19:19:16 +0330

    add markdown_format (custome filter)

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from ..models import Post
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -11,3 +13,7 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts':latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
