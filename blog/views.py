0b9dea8e600745315de1af1e148ee23928375b4a
Boshra Irandoust <boshra.irnd@gmail.com>
2021-11-12 16:17:11 +0330

    add SearchRank

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector ,SearchQuery ,SearchRank
 # Create your views here.
 
 
@@ -99,5 +99,7 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            resault = Post.published.annotate(search = SearchVector('title','body')).filter(search=query)   
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
+            resault = Post.published.annotate(search=search_vector,rank=SearchRank(search_vector,search_query)).filter(search=search_query).order_by('-rank')  
     return render(request, 'blog/post/search.html',{'form':form,'query':query,'resaults':resault}) 
